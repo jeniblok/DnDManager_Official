0deb1860196b9aef6e52d37b6cb4c92b42103f2c
Benjamin Terzic <52902523+CloneMalone@users.noreply.github.com>
2026-04-25 19:05:16 -0500

    Implement Index action in CampaignController
    Added Index action to CampaignController.

--- Controllers/CampaignController.cs
+++ Controllers/CampaignController.cs
@@ -5,13 +5,17 @@ namespace DnDManager.Controllers
 {
     public class CampaignController : Controller
     {
-        private readonly AppDbContext _context;
+        // private readonly AppDbContext _context;
 
-        public CampaignController(AppDbContext context)
-        {
-            _context = context;
-        }
+        //public CampaignController(AppDbContext context)
+        //{
+        //    _context = context;
+        //}
 
         // Actions go here
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
     }
 }
