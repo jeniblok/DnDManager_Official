b5e6134a8958cf7382a2eef2d5e15b97acc06291
jeniblok <116460893+jeniblok@users.noreply.github.com>
2026-04-30 09:15:10 -0500

    Update LogEntry.cs
    Fixed version since my last update

--- Models/LogEntry.cs
+++ Models/LogEntry.cs
@@ -3,13 +3,17 @@ using System.ComponentModel.DataAnnotations;
 namespace DnDManager.Models
 {
     public class LogEntry
-{
-    public int Id { get; set; }
-    public int RoundNumber { get; set; }
-    public string Description { get; set; }
-    public int? DamageOrHealing { get; set; }
+    {
+        public int Id { get; set; }
 
-    public int EncounterId { get; set; }
-    public Encounter Encounter { get; set; }
+        public int RoundNumber { get; set; }
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+
+        public int? DamageOrHealing { get; set; }
+
+        public int EncounterId { get; set; }
+        public Encounter? Encounter { get; set; }
     }
 }
