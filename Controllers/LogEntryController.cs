bb82c5266eec1cacc5ea345cf9c9be291cf39db7
jeniblok <116460893+jeniblok@users.noreply.github.com>
2026-04-30 09:17:41 -0500

    Update LogEntryController.cs
    updated

--- Controllers/LogEntryController.cs
+++ Controllers/LogEntryController.cs
@@ -1,5 +1,6 @@
 using DnDManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DnDManager.Controllers
 {
@@ -12,6 +13,96 @@ namespace DnDManager.Controllers
             _context = context;
         }
 
-        // Actions go here
+        public IActionResult Index()
+        {
+            var logEntries = _context.LogEntries
+                .Include(l => l.Encounter)
+                .ToList();
+
+            return View(logEntries);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var logEntry = _context.LogEntries
+                .Include(l => l.Encounter)
+                .FirstOrDefault(l => l.Id == id);
+
+            if (logEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(logEntry);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(LogEntry logEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.LogEntries.Add(logEntry);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(logEntry);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var logEntry = _context.LogEntries.Find(id);
+
+            if (logEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(logEntry);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(LogEntry logEntry)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.LogEntries.Update(logEntry);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(logEntry);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var logEntry = _context.LogEntries.Find(id);
+
+            if (logEntry == null)
+            {
+                return NotFound();
+            }
+
+            return View(logEntry);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var logEntry = _context.LogEntries.Find(id);
+
+            if (logEntry != null)
+            {
+                _context.LogEntries.Remove(logEntry);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
