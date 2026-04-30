536ab9d2b65a4544ea417f4f6f1338f777606e9d
jeniblok <116460893+jeniblok@users.noreply.github.com>
2026-04-30 09:10:42 -0500

    Update Encounter.cs
    Updated

--- Models/Encounter.cs
+++ Models/Encounter.cs
@@ -3,7 +3,14 @@ using System.ComponentModel.DataAnnotations;
 namespace DnDManager.Models
 {
     public class Encounter
-    {
-        // Properties go here
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Outcome { get; set; }
+
+    public int? SessionLogId { get; set; }
+    public SessionLog? SessionLog { get; set; }
+
+    public List<LogEntry> LogEntries { get; set; } = new List<LogEntry>();
     }
 }
