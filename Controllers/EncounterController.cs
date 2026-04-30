3feccdcb1be4f4eddeb8b61f30f625fe9801f1b6
jeniblok <116460893+jeniblok@users.noreply.github.com>
2026-04-30 09:16:33 -0500

    Update EncounterController.cs
    Updated

--- Controllers/EncounterController.cs
+++ Controllers/EncounterController.cs
@@ -1,5 +1,6 @@
 using DnDManager.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DnDManager.Controllers
 {
@@ -12,6 +13,97 @@ namespace DnDManager.Controllers
             _context = context;
         }
 
-        // Actions go here
+        public IActionResult Index()
+        {
+            var encounters = _context.Encounters
+                .Include(e => e.SessionLog)
+                .ToList();
+
+            return View(encounters);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var encounter = _context.Encounters
+                .Include(e => e.SessionLog)
+                .Include(e => e.LogEntries)
+                .FirstOrDefault(e => e.Id == id);
+
+            if (encounter == null)
+            {
+                return NotFound();
+            }
+
+            return View(encounter);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Encounter encounter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Encounters.Add(encounter);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(encounter);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var encounter = _context.Encounters.Find(id);
+
+            if (encounter == null)
+            {
+                return NotFound();
+            }
+
+            return View(encounter);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Encounter encounter)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Encounters.Update(encounter);
+                _context.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(encounter);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var encounter = _context.Encounters.Find(id);
+
+            if (encounter == null)
+            {
+                return NotFound();
+            }
+
+            return View(encounter);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var encounter = _context.Encounters.Find(id);
+
+            if (encounter != null)
+            {
+                _context.Encounters.Remove(encounter);
+                _context.SaveChanges();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
